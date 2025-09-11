67f8b62d8704b281b6c731c62b271442b0e1981d
A-Ijishakin <ayodeji.ijishakin.21@ucl.ac.uk>
2025-09-11 15:34:24 +0100

    Cleaned Up UnSubscribe API

--- spread_sheet_utils.py
+++ spread_sheet_utils.py
@@ -2,7 +2,6 @@ class SpreadSheetUtils:
     def __init__(self, sheet):
         self.sheet = sheet
 
-
     def get_col_index(self, column_name):
         header = self.sheet.row_values(1)
         return header.index(column_name) + 1
@@ -10,6 +9,6 @@ class SpreadSheetUtils:
     def find_row_by_col_value(self, column_name, value_to_find):
         all_rows = self.sheet.get_all_records()
         for idx, row in enumerate(all_rows):
-            if row.get(column_name) == value_to_find:
+            if str(row.get(column_name)) == str(value_to_find):
                 return idx + 2  # +2 accounts for header row (1) + 0-based index
         return None
